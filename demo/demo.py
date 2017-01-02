99d737b238ac2a10244644274fe2dffbd7f146b3
Liu Qun <517067180@qq.com>
2017-01-02 14:55:05 +0800

    《社交网站的数据挖掘》章节7.2
    获取命令行GitHub的API令牌

---
+++ demo/demo.py
@@ -0,0 +1,25 @@
+﻿import requests
+from getpass import getpass
+import json
+
+# 获取 GitHub 个人API访问令牌的实例代码
+# Create an OAuth token for command-line use
+# http://bit.ly/lalo7lG
+
+username= 'liuqun' # My GitHub acount
+password= '******' # My password
+url= 'https://api.github.com/authorizations'
+note= 'Mining the Social Web, 2nd Ed.'
+post_data= {'scopes':['repo'], 'note': note}
+data= json.dumps(post_data)
+response= requests.post(
+		url,
+		auth= (username, password),
+		data,
+	)
+print("", response.text)
+print("")
+print("Your OAuth token is", response.json()['token'])
+
+# Go to https://github.com/settings/applications to revoke this token
+
